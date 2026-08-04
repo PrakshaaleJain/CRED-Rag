d485898b0208e06eb7f95bb53fcf49a103a06125
PrakshaaleJain <prakshaale123@gmail.com>
2026-08-04 16:24:19 +0530

    deleted micro-scripts

--- test_eval2.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv('OmegaSquared/data/CRED/filteredpromax_kaggle_with_year_month_CRED.csv')
-y = df['label'].values
-print("y dtype:", y.dtype)
-print("y[0] type:", type(y[0]))
